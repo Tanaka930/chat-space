268f4b9f30a541051e67725203729e4bd3010955
Yuma Tanaka <yumat9637@gmail.com>
2019-10-09 04:39:39 +0900

    非同期

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function() {
+
+  function buildPost(post){
+    var html = `<div class="message">
+                  <div class="message__user">
+                    ${post.user_name}
+                  </div>
+                  <div class="message__time">
+                    ${post.created_at}
+                  </div>
+                  <div class="lower-message">
+                    <div class="lower-message__text">
+                      ${post.content}
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(post) {
+      var html = buildPost(post);
+      $('.messages').append(html)
+      $('#message_content').val('')
+      $('.form__submit').attr('disabled', false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      return false
+    })
+    .fail(function() {
+      alert('エラー');
+    })
+  })
+});  
