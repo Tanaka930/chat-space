825203a7bccf4e344f21b8b37c8646b6fe153655
Yuma Tanaka <yumat9637@gmail.com>
2019-10-14 19:12:59 +0900

    非同期通信　修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,24 +3,24 @@ $(function() {
   function buildHTML(message){
     var image = message.image ? `<img src = '${message.image}'>` : "";
     var html =
-      `<div class="message" data-message-id="${message.id}">
-         <div class="upper-message">
-           <div class="upper-message__user">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__time">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__text">
-             ${message.content}
-           </p>
-           <div class="lower-message__image">
-             ${image}
-           </div>
-         </div>
-       </div>`
+     `<div class="message" data-id="${message.id}">
+        <div class="upper-message">
+          <div class="upper-message__user">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__time">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__text">
+            ${message.content}
+          </p>
+          <div class="lower-message__image">
+            ${image}
+          </div>
+        </div>
+      </div>`
     return html
   }
 
@@ -48,27 +48,39 @@ $(function() {
     return false;
   })
 
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      last_message_id = $('.message:last').data("message-id");
+
+  // 自動更新
+  $(function() {
+    $(function() {
+      if (location.href.match(/\/groups\/\d+\/messages/)) {
+        setInterval(update, 5000);
+      }
+    });
+    function update(){
+      if($('.message')[0]){
+        var message_id = $('.message:last').data('id');
+      } else {
+        return false
+      }
+
       $.ajax({
         url: "api/messages",
         type: 'GET',
-        dataType: 'json',
-        data: {id: last_message_id}
+        data: { id : message_id },
+        dataType: 'json'
       })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function (message) {
-        insertHTML = buildHTML(message);
-        $('.messages').append(insertHTML); 
-        $('.messages').animate({scrollTop: $('.messages').height()})
-        })
+      .done(function(data){
+        if (data.length){
+          $.each(data, function(index,data){
+            var html = buildHTML(data);
+            $('.messages').append(html)
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+          })
+        }
       })
-      .fail(function() {
-        alert('更新に失敗しました');
-      });
-    };
-  } 
-  setInterval(reloadMessages, 5000); 
-});  
+      .fail(function(){
+        console.log('自動更新に失敗しました')
+      })
+    }
+  })
+});
